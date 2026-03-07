d2b124fbe00e47e4d0dcda065a03d182d909b1f1
pomelo233 <234902650@qq.com>
2026-03-08 07:15:13 +0800

    ceshi

--- plugins/cnlangsigninV2/__init__.py
+++ plugins/cnlangsigninV2/__init__.py
@@ -31,7 +31,7 @@ class CnlangSigninV2(_PluginBase):
     # 作者主页
     author_url = "https://github.com/imaliang"
     # 插件配置项ID前缀
-    plugin_config_prefix = "cnalngsignin_"
+    plugin_config_prefix = "cnalngsigninv2_"
     # 加载顺序
     plugin_order = 2
     # 可使用的用户级别
