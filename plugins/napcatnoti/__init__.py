88b060d255fd173f21f13cf02592b093cb189c4a
pomelo233 <234902650@qq.com>
2026-03-08 08:05:26 +0800

    sss

--- plugins/napcatnoti/__init__.py
+++ plugins/napcatnoti/__init__.py
@@ -26,7 +26,7 @@ class NapcatNoti(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "napcatnoti_"
     # 加载顺序
-    plugin_order = 6
+    plugin_order = 1
     # 可使用的用户级别
     auth_level = 1
 
